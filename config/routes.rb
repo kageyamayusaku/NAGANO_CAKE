0e27df2a6eff4960fc618b0f42859a3a67950e34
EC2 Default User <ec2-user@ip-172-31-2-145.ap-northeast-1.compute.internal>
2023-02-06 13:19:54 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,25 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
-  # public routes #
-  root to: 'customer/homes#top'
-
+  scope module: :public do
+    root to: "homes#top"
+    get 'about' => "homes#about"
+    resources :items, only: [:index, :show]
+    get 'customers/my_page' => "customers#show"
+    get 'customers/information/edit' => "customers#edit"
+    patch 'customers/information' => "customers#update"
+    get 'customers/unsubscribe' => "customers#unsubscribe"
+    patch 'customers/withdraw' => "customers#withdraw"
+    resources :cart_items, except: [:new, :show, :edit]
+    delite 'cart_items/destroy_all' => "cart_items#destroy_all"
+    resources :orders, except: [:edit, :update, :destroy]
+    post 'orders/confirm' => "orders#comfirm"
+    get 'orders/complete' => "orders#complete"
+    resources :addresses, except: [:new, :show]
+  end
 
   namespace :admin do
-    get '/' => "homes#top"
+    root to: "homes#top"
     resources :items, except: [:destroy]
     resources :genres, except: [:new, :show, :destroy]
     resources :customers, except: [:new, :create, :destroy]
