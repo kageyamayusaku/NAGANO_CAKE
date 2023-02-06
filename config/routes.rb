1d0a759e1c1e9b3f4518d669ec367e44c3bbe3c8
EC2 Default User <ec2-user@ip-172-31-2-145.ap-northeast-1.compute.internal>
2023-02-06 10:07:34 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,25 @@
 Rails.application.routes.draw do
+
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-
-
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
 
+  # public routes #
   root to: 'customer/homes#top'
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    get '/' => "homes#top"
+    resources :items, except: [:destroy]
+    resources :genres, except: [:new, :show, :destroy]
+    resources :customers, except: [:new, :create, :destroy]
+    get 'orders' => "orders#show"
+    patch 'orders' => "orders#update"
+    patch 'order_details' => "order_details#update"
+  end
+
 end
