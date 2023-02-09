7cca36bde2a76b09dae163c11edf76dbc7f6e645
EC2 Default User <ec2-user@ip-172-31-2-145.ap-northeast-1.compute.internal>
2023-02-09 21:59:42 +0000

    [Update]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,20 +1,35 @@
 class Admin::ItemsController < ApplicationController
-  def index
-  end
-
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to admin_item_path(@item.id)
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  def index
   end
 
   def edit
   end
 
   def update
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price, :is_active)
   end
 
 end
