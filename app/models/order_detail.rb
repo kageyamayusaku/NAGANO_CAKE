ab2cb3cb577563880d16f80823c1bfc69bd77b1c
EC2 Default User <ec2-user@ip-172-31-2-145.ap-northeast-1.compute.internal>
2023-02-17 01:38:19 +0000

    [Update]

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,10 +1,10 @@
 class OrderDetail < ApplicationRecord
 
   enum making_status:{
-    impossible: 0,
-    waiting: 1,
-    production: 2,
-    completion: 3
+    製作不可: 0,
+    製作待ち: 1,
+    製作中: 2,
+    製作完了: 3
   }
 
   belongs_to :order
