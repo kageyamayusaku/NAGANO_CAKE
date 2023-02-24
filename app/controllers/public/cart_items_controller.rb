da4d8be7285319f50c42d9fb5e9353efa7bfacfe
EC2 Default User <ec2-user@ip-172-31-2-145.ap-northeast-1.compute.internal>
2023-02-24 12:55:55 +0000

    [Update]ここからがんばる！！

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,6 +2,8 @@ class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
 
   def index
+    @cart_items = CartItem.all
+    @total = @cart_items.inject(0) { |sum, item| sum + subtotal }
   end
 
   def update
@@ -14,5 +16,13 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = CartItem.new(cart_item_params)
   end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount, :customer_id)
+  end
+
 end
