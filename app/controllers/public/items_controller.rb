9534bcfb407cc2faf5f911a49eeb941e92eb4aa6
EC2 Default User <ec2-user@ip-172-31-2-145.ap-northeast-1.compute.internal>
2023-02-24 01:27:52 +0000

    [Update]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,11 @@
 class Public::ItemsController < ApplicationController
   def index
+    @genres = Genre.all
+    @items = Item.all
+    @items2 = Item.page(params[:page]) # kaminariのmethod #
   end
 
   def show
+
   end
 end
