f5632f0d04a663627da10bd551a3011c87d0631f
EC2 Default User <ec2-user@ip-172-31-2-145.ap-northeast-1.compute.internal>
2023-03-09 10:18:53 +0000

    [Fix]customer修正

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_permitted_parameters, only: [:create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :configure_account_update_params, only: [:update]
 
   def after_sign_up_path_for(resource)
@@ -9,9 +9,9 @@ class Public::RegistrationsController < Devise::RegistrationsController
   end
 
   # GET /resource/sign_up
-  def new
-    @customer = Customer.new
-  end
+  #def new
+  #  @customer = Customer.new
+  #end
 
   # POST /resource
   # def create
@@ -65,7 +65,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email])
   end
 
 end
