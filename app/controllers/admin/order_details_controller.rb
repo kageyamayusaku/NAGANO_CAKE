9d689f8ed25ab7e5353eba0af9d0b5edcf5b5ca5
EC2 Default User <ec2-user@ip-172-31-2-145.ap-northeast-1.compute.internal>
2023-03-09 20:48:14 +0000

    [Finish]完成！

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -5,7 +5,7 @@ class Admin::OrderDetailsController < ApplicationController
     order_detail = OrderDetail.find(params[:id])
     if order_detail.update(order_detail_params)
       flash[:notice] = ""
-      redirect_to admin_order_path(@order.id)
+      redirect_to admin_order_path(order_detail.order.id)
     else
       render 'admin/orders/show'
     end
